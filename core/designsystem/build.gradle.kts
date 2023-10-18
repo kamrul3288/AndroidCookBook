3a835edcf456f7338c5863aba89a3e4f81706bca
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-10-18 07:13:20 +0600

    add typography.xml

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
     implementation(libs.androidx.corektx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.material)
+    implementation(libs.dimension.sdp)
+    implementation(libs.dimension.ssp)
+
     testImplementation(libs.test.junit)
     androidTestImplementation(libs.test.extjunit)
     androidTestImplementation(libs.test.espresso)
