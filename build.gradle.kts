d31f39e2b64cc3f0cd207751260de09f67639cae
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-10-18 14:45:16 +0600

    add navigation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidx.navigation.safeargs) apply false
 }
 true
