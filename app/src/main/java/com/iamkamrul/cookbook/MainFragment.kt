f58efd07d54c8b63c70b7ebc1a1dc2f1150ee2ad
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-10-29 12:16:15 +0600

    implement home screen

--- app/src/main/java/com/iamkamrul/cookbook/MainFragment.kt
+++ app/src/main/java/com/iamkamrul/cookbook/MainFragment.kt
@@ -2,13 +2,28 @@ package com.iamkamrul.cookbook
 
 import android.os.Bundle
 import com.iamkamrul.cookbook.databinding.FragmentMainBinding
+import com.iamkamrul.ui.adapter.FeaturesComponentAdapter
 import com.iamkamrul.ui.base.BaseFragment
+import com.iamkamrul.ui.extension.setUpGridRecyclerViewAdapter
 
 class MainFragment : BaseFragment<FragmentMainBinding>() {
     override fun viewBindingLayout(): FragmentMainBinding  =
         FragmentMainBinding.inflate(layoutInflater)
 
-    override fun initializeView(savedInstanceState: Bundle?) {
+    private val features = listOf("Navigation", "Material Component")
+    private val adapter by lazy { FeaturesComponentAdapter()}
 
+    override fun initializeView(savedInstanceState: Bundle?) {
+        configureTopAppbar()
+        configureRecyclerViewWithAdapter()
+    }
+
+    private fun configureRecyclerViewWithAdapter(){
+        requireContext().setUpGridRecyclerViewAdapter(binding.componentRv,adapter,2)
+        adapter.submitList(features)
+    }
+
+    private fun configureTopAppbar(){
+        binding.topAppBarLayout.topAppBar.title = "Home"
     }
 }
