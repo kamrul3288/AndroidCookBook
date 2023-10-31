663a3f26641ad5dc5e9ddc5cfb18a19b2efa13d2
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-10-31 13:29:01 +0600

    add navigation animation

--- core/ui/src/main/java/com/iamkamrul/ui/extension/NavigationComponent.kt
+++ core/ui/src/main/java/com/iamkamrul/ui/extension/NavigationComponent.kt
@@ -3,6 +3,7 @@ package com.iamkamrul.ui.extension
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.iamkamrul.ui.R
@@ -27,5 +28,32 @@ fun Fragment.navigate(
                 }
             }
         }
+    )
+}
+
+fun Fragment.navigate(direction: NavDirections) {
+    val currentNode = (
+            if (findNavController().currentBackStack.value.isEmpty()) findNavController().graph
+            else findNavController().currentBackStack.value.last().destination
+            )
+    val navAction = currentNode.getAction(direction.actionId)
+    findNavController().navigate(
+        direction.actionId,
+        direction.arguments,
+        navAction?.navOptions?.let {
+            navOptions {
+                anim {
+                    enter = R.anim.slide_in_right
+                    exit = R.anim.slide_out_left
+                    popEnter = R.anim.slide_in_left
+                    popExit = R.anim.slide_out_right
+                }
+                popUpTo(it.popUpToId){
+                    inclusive = it.isPopUpToInclusive()
+                }
+
+            }
+        }
+
     )
 }
