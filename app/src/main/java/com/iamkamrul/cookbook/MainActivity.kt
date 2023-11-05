b2f25ad41255cfa3f08c0dd67f02aa329d50c0c4
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-11-05 18:07:58 +0600

    implement implicit deeplink

--- app/src/main/java/com/iamkamrul/cookbook/MainActivity.kt
+++ app/src/main/java/com/iamkamrul/cookbook/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.iamkamrul.cookbook
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.iamkamrul.cookbook.databinding.ActivityMainBinding
 import com.iamkamrul.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-    override fun viewBindingLayout(): ActivityMainBinding =
-        ActivityMainBinding.inflate(layoutInflater)
+    override fun viewBindingLayout() = ActivityMainBinding.inflate(layoutInflater)
 
+    private lateinit var navController:NavController
     override fun initializeView(savedInstanceState: Bundle?) {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.mainNavHost.id) as NavHostFragment
+        navController = navHostFragment.navController
+        onNewIntent(intent)
+    }
 
+    //implement implicit deeplink example.com/kamrul
+    // then navigate to singup fragment
+    @SuppressLint("MissingSuperCall")
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navController.handleDeepLink(intent)
     }
 
 }
